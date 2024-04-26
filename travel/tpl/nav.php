9a65658eeb053923fd37ace7618189292c94ed85
IAmIngibitor <109351663+IAmIngibitor@users.noreply.github.com>
2024-04-26 19:27:23 +0700

    Delete travel directory

--- travel/tpl/nav.php
+++
@@ -1,7 +0,0 @@
-<div class="container" id="menu">
-  <nav class="nav nav-pills flex-column flex-sm-row">
-    <a class="flex-sm-fill text-sm-center nav-link" href="index.php">Главная</a>
-    <a class="flex-sm-fill text-sm-center nav-link" href="tours.php">Наши туры</a>
-    <a class="flex-sm-fill text-sm-center nav-link" href="contacts.php">О нас</a>
-  </nav>
-</div>
